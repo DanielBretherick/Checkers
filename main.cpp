354dde60c7b535427bc4c4d91e961538d277952b
Cuadros99 <dany@Cuadros-Desktop.localdomain>
2022-06-12 16:32:13 -0300

    feature: implement jogar() method

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 //Jogo de Damas
-
-
 #include <iostream>
 #include <math.h>
 #include <vector>
 #include <algorithm>
+
+#define WARNING "*** ---"
 
 
 using namespace std;
@@ -14,7 +14,7 @@ enum tiposPeca {COMUM, RAINHA, VAZIO};
 enum colunas {A, B, C, D, E, F, G, H};
 
 vector<char> letrasColunas({'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}) ;
-char letraCores[2] = {'B', 'P'};
+char letraCores[4] = {'b', 'p', 'B', 'P'};
 
 class Peca;
 class Comum;
@@ -49,6 +49,8 @@ public:
     return cor;
   }
   tiposPeca getTipo() {return tipo;}
+  static int getNumBrancas() { return numBrancas; }
+  static int getNumPretas() { return numPretas; }
   virtual bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {return false;};
 };
 
@@ -63,6 +65,7 @@ public:
 class Comum: public Peca{
 public:
   Comum(cores cor): Peca(COMUM, cor) {}
+  Comum(const Comum &c): Peca(c.tipo, c.cor) {}
   bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb);
 };
 
@@ -104,7 +107,7 @@ public:
           cout << "  ";
         else {
           cores cor = tabuleiro[i][j]->getCor();
-          cout << " " << letraCores[cor];
+          cout << " " << (tabuleiro[i][j]->getTipo() == RAINHA ? letraCores[cor+2] : letraCores[cor]) ;
         }
         if(j+1 < 8)
           cout << " |";
@@ -117,94 +120,138 @@ public:
     cout << "    ";
     for(int k=0; k<8; k++)
       cout << letrasColunas[k] << "   ";
-    cout << endl;
+    cout << endl << endl;
+    cout << "Nº de peças BRANCAS: " << Peca::getNumBrancas() << endl;
+    cout << "Nº de peças PRETAS: " << Peca::getNumPretas();
+    cout << endl << endl;
   }
 
   void transformarCoordenadas(int &li, char &ci, int &lf, char &cf){
-   int i = count(letrasColunas.begin(), letrasColunas.end(), ci);
-   int j = count(letrasColunas.begin(), letrasColunas.end(), cf);
-   if(i == 0 || j == 0)
-    cout << "*** --- Letra invalida --- ***"<< endl;
-   else{
-    for(int j = 0; j<8; j++){
-      if(ci == letrasColunas[j])
-        ci = j;
-      if(cf == letrasColunas[j])
-        cf = j;
-    }
-     li = 8 - li;
-     lf = 8 - lf;
-  }
-
-  void jogar(int li, char ci, int lf, char cf) {
-    transformarCoordenadas(li, ci, lf, cf);
-
-    if(!verificarJogada(lit,cit,lft,cft)) {
-      cout << "Jogada inválida" << endl;
-    }
-      // Equals the destination and the moved piece
-      // Equals the inicial space to NULL
-      // Delete the taken piece, if it exists, and equal its old space to NULL
-
-      tabuleiro[lft][cft] = tabuleiro[lit][cit];
-      tabuleiro[lit][cit] = NULL;
-
-      int sinalL = (lft-lit)/abs((lft-lit));
-      int sinalC = (cft-cit)/abs((cft-cit));
-
-      if(tabuleiro[lft-sinalL][cft-sinalC] != NULL) {
-        delete tabuleiro[lft-sinalL][cft-sinalC];
-        tabuleiro[lft-sinalL][cft-sinalC] = NULL;
+    int i = count(letrasColunas.begin(), letrasColunas.end(), ci);
+    int j = count(letrasColunas.begin(), letrasColunas.end(), cf);
+    if(i == 0 || j == 0)
+      cout << "*** --- Letra invalida --- ***"<< endl;
+    else {
+      for(int j = 0; j<8; j++){
+        if(ci == letrasColunas[j])
+          ci = j;
+        if(cf == letrasColunas[j])
+          cf = j;
       }
-
-   }
+      li = 8 - li;
+      lf = 8 - lf;
+      ci = (int)ci;
+      cf = (int)cf;
+    }
   }
 
-  bool verificarJogada(int lit, int cit, int lft, int cft) {
-    // 1. Verificar limites do tabuleiro
-    // 2. Verificar se há uma peça do seu time no local
-    // 3. VerificarDestino()
+  void solicitarJogada() {
+    int li, lf;
+    char cci, ccf;
+    cout << "\n********** TURNO DO JOGADOR " << letraCores[proxJogador+2] << " **********\n" << endl;
+    cout << "Escolha a posição da peça a ser movida: ";
+    cin >> li >> cci;
 
-    if(lit > 8 || cit > 8 || lft > 8 || cft > 8 || lit <= 0 || cit < 0 || lft <= 0 || cft < 0) {
+    if(cci >= 'a' && cci <= 'z') {
+      cci+= ('A'-'a');
+    }
+
+    cout << "Escolha o destino do movimento: ";
+    cin >> lf >> ccf;
+    cout << endl;
+    
+    if(ccf >= 'a' && ccf <= 'z') {
+      ccf+= ('A'-'a');
+    }
+
+    jogar(li, cci, lf, ccf);
+  }
+
+  void jogar(int li, char cci, int lf, char ccf) {
+    transformarCoordenadas(li, cci, lf, ccf);
+    int ci = (int)cci;
+    int cf = (int)ccf;
+    if(!verificarJogada(li,ci,lf,cf)) {
+      cout << "!!!!!!!!! Insira outra jogada !!!!!!!!!\n" << endl;
+      solicitarJogada();
+    }
+    else{
+      tabuleiro[lf][cf] = tabuleiro[li][ci];
+      tabuleiro[li][ci] = NULL;
+
+      int sinalL = (lf-li)/abs((lf-li));
+      int sinalC = (cf-ci)/abs((cf-ci));
+
+      if(tabuleiro[lf-sinalL][cf-sinalC] != NULL) {
+        delete tabuleiro[lf-sinalL][cf-sinalC];
+        tabuleiro[lf-sinalL][cf-sinalC] = NULL;
+      }
+      imprimirTabuleiro();
+      proxJogador = (proxJogador == BRANCA) ? PRETA : BRANCA;
+      solicitarJogada();
+    }
+   }
+
+  bool verificarJogada(int li, int ci, int lf, int cf) {
+    if(li > 8 || ci > 8 || lf > 8 || cf > 8 || li <= 0 || ci < 0 || lf <= 0 || cf < 0) {
       cout << "*** --- Posicao fora do Tabuleiro --- ***" << endl;
       return false;
     }
     else {
-      if(tabuleiro[lit][cit] == NULL)
+      if(tabuleiro[li][ci] == NULL){
+        cout << "*** --- Posição sem peças --- ***" << endl;
         return false;
+        }
       else
-        if(tabuleiro[lit][cit]->getCor() != proxJogador)
+        if(tabuleiro[li][ci]->getCor() != proxJogador){
+          cout << "*** --- Peça de outro jogador --- ***" << endl;
           return false;
+          }
     }
-
-    return tabuleiro[lit][cit]->verificarDestino(lit, cit, lft, cft, *this);
+    return tabuleiro[li][ci]->verificarDestino(li, ci, lf, cf, *this);
   }
 };
 
 bool Comum::verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
-    // 1. Movimento é diagonal
-    // 2. Se for 1 casas, verificar se há peças no destino
-    // 3. Se for 2 casas, verificar se há peça inimiga no caminho e se há peças no destino
-    int dl = lf -li;
-    int dc = cf -ci;
-    if(dl<0) {return false;}
-    if(tb.tabuleiro[lf][cf] != NULL) {return false;}
-    if(dl == 1 && abs(dc) == 1) {return true;}
-    if(dl == 2 && abs(dc) == 2) {
-      int sinal = dc/abs(dc);
-      if(tb.tabuleiro[li+1][ci+sinal] != NULL) {
-        if(tb.tabuleiro[li+1][ci+sinal]->getCor() != cor)
-          return true;
-      }
-    }
+  int dl = li -lf;
+  int dc = cf -ci;
+  if(abs(dl) != abs(dc)) { 
+    cout << "*** --- Movimento não foi diagonal --- ***" << endl;
     return false;
   }
+  if((dl<0 && (cor == BRANCA))  || (dl > 0 && (cor == PRETA)) ) {
+    cout << "*** --- Indo para tras --- ***" << endl; 
+    return false;
+  }
+  if(tb.tabuleiro[lf][cf] != NULL) {
+    cout << "*** --- Posicao ocupada --- ***" << endl;
+    return false;
+  }
+  if(abs(dl) == 1 && abs(dc) == 1) {return true;}
+  if(abs(dl) == 2 && abs(dc) == 2) {
+    int sinalC = dc/abs(dc);
+    int sinalL = -dl/abs(dl);
+    if(tb.tabuleiro[li+sinalL][ci+sinalC] == NULL) {
+      cout << "*** --- Nao pode pular duas, espaco vazio --- ***" << endl;
+      return false;
+    }
+    else{
+      Peca *aux = tb.tabuleiro[li+sinalL][ci+sinalC];
+      if(aux->getCor() == cor){
+        cout << "*** --- Nao pode comer a própria peca --- ***" << endl;
+        return false;
+      }
+      return true;
+    }
+
+  }
+  else{
+    cout << "*** --- Mais de duas casas andadas --- ***" << endl;
+    return false;
+  }
+}
 
 bool Rainha::verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
-    // 1. Movimento é diagonal
-    // 2. Verificar se há peça no destino final
-    // 3. Se for so 1 casa dar ok
-    // 4. Se for mais de 1 casa, verificar se há peças inimigas no caminho
     int dl = lf -li;
     int dc = cf -ci;
     if(abs(dl)!=abs(dc)) {return false;}
@@ -212,7 +259,7 @@ bool Rainha::verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
     if(abs(dl) == 1 && abs(dc) == 1) {return true;}
     if(abs(dl) > 1 && abs(dc) > 1) {
       int sinalC = dc/abs(dc);
-      int sinalL = dl/abs(dl);
+      int sinalL = -dl/abs(dl);
       for(int l=lf-2*sinalL,c=cf-2*sinalC ; l<lf ; ) {
         if(tb.tabuleiro[l][c] != NULL) {
           return false;
@@ -232,8 +279,7 @@ int main(){
 
   Tabuleiro tab;
   tab.imprimirTabuleiro();
-  cout << endl;
-  tab.jogar(1,'A', 2,'C');
+  tab.solicitarJogada();
 
 
   return 0;

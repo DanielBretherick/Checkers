08414cee10fc08e33529d3e5f367b8a3e00a22a2
Cuadros99 <dany@Cuadros-Desktop.localdomain>
2022-06-11 21:01:30 -0300

    bugfix: declare the methods verificarDestino() below the Tabuleiro class declaraction

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ enum colunas {A, B, C, D, E, F, G, H};
 vector<char> letrasColunas({'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}) ;
 char letraCores[2] = {'B', 'P'};
 
+class Peca;
+class Comum;
+class Rainha;
+
+class Tabuleiro;
+
 class Peca{
 protected:
   static int numPretas, numBrancas;
@@ -45,7 +51,7 @@ public:
     void setCor(cores val){
       cor = val;
     }
-    virtual bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {};
+    virtual bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {return false;};
 };
 
 int Peca::numPretas = 0;
@@ -54,57 +60,13 @@ int Peca::numBrancas = 0;
 class Rainha: public Peca {
 public:
   Rainha(cores cor): Peca(RAINHA, cor) {}
-  bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
-    // 1. Movimento é diagonal
-    // 2. Verificar se há peça no destino final
-    // 3. Se for so 1 casa dar ok
-    // 4. Se for mais de 1 casa, verificar se há peças inimigas no caminho
-    int dl = lf -li;
-    int dc = cf -ci;
-    if(abs(dl)!=abs(dc)) {return false;}
-    if(tb.tabuleiro[lf][cf] != NULL) {return false;}
-    if(abs(dl) == 1 && abs(dc) == 1) {return true;}
-    if(abs(dl) > 1 && abs(dc) > 1) {
-      int sinalC = dc/abs(dc);
-      int sinalL = dl/abs(dl);
-      for(int l=lf-2*sinalL,c=cf-2*sinalC ; l<lf ; ) {
-        if(tb.tabuleiro[l][c] != NULL) {
-          return false;
-        }
-        l+=sinalL;
-        c+=sinalC;
-      }
-      if(tb.tabuleiro[lf-sinalL][cf-sinalC] != NULL) {
-        if(tb.tabuleiro[lf-sinalL][cf-sinalC]->getCor() != cor)
-          return true;
-      }
-    }
-    return false;
-  }
+  bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb);
 };
 class Comum: public Peca{
 public:
   Comum(cores cor): Peca(COMUM, cor) {}
-  bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
-    // 1. Movimento é diagonal
-    // 2. Se for 1 casas, verificar se há peças no destino
-    // 3. Se for 2 casas, verificar se há peça inimiga no caminho e se há peças no destino
-    int dl = lf -li;
-    int dc = cf -ci;
-    if(dl<0) {return false;}
-    if(tb.tabuleiro[lf][cf] != NULL) {return false;}
-    if(dl == 1 && abs(dc) == 1) {return true;}
-    if(dl == 2 && abs(dc) == 2) {
-      int sinal = dc/abs(dc);
-      if(tb.tabuleiro[li+1][ci+sinal] != NULL) {
-        if(tb.tabuleiro[li+1][ci+sinal]->getCor() != cor)
-          return true;
-      }
-    }
-    return false;
-  }
+  bool verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb);
 };
-
 
 class Tabuleiro{
   int proxJogador;
@@ -171,6 +133,53 @@ public:
     return true;
   }
 };
+
+bool Comum::verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
+    // 1. Movimento é diagonal
+    // 2. Se for 1 casas, verificar se há peças no destino
+    // 3. Se for 2 casas, verificar se há peça inimiga no caminho e se há peças no destino
+    int dl = lf -li;
+    int dc = cf -ci;
+    if(dl<0) {return false;}
+    if(tb.tabuleiro[lf][cf] != NULL) {return false;}
+    if(dl == 1 && abs(dc) == 1) {return true;}
+    if(dl == 2 && abs(dc) == 2) {
+      int sinal = dc/abs(dc);
+      if(tb.tabuleiro[li+1][ci+sinal] != NULL) {
+        if(tb.tabuleiro[li+1][ci+sinal]->getCor() != cor)
+          return true;
+      }
+    }
+    return false;
+  }
+
+bool Rainha::verificarDestino(int li, int ci, int lf, int cf, Tabuleiro &tb) {
+    // 1. Movimento é diagonal
+    // 2. Verificar se há peça no destino final
+    // 3. Se for so 1 casa dar ok
+    // 4. Se for mais de 1 casa, verificar se há peças inimigas no caminho
+    int dl = lf -li;
+    int dc = cf -ci;
+    if(abs(dl)!=abs(dc)) {return false;}
+    if(tb.tabuleiro[lf][cf] != NULL) {return false;}
+    if(abs(dl) == 1 && abs(dc) == 1) {return true;}
+    if(abs(dl) > 1 && abs(dc) > 1) {
+      int sinalC = dc/abs(dc);
+      int sinalL = dl/abs(dl);
+      for(int l=lf-2*sinalL,c=cf-2*sinalC ; l<lf ; ) {
+        if(tb.tabuleiro[l][c] != NULL) {
+          return false;
+        }
+        l+=sinalL;
+        c+=sinalC;
+      }
+      if(tb.tabuleiro[lf-sinalL][cf-sinalC] != NULL) {
+        if(tb.tabuleiro[lf-sinalL][cf-sinalC]->getCor() != cor)
+          return true;
+      }
+    }
+    return false;
+  }
 
 int main(){
 
